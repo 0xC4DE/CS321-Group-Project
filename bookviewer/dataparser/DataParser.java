c823364b630e83cb621b9ca28dea1ab1f3bf0c9c
Joshua Cantrell <jc0248@uah.edu>
2022-02-09 16:39:26 -0600

    Java module ting

---
+++ bookviewer/dataparser/DataParser.java
@@ -0,0 +1,15 @@
+package bookviewer.dataparser;
+
+public class DataParser {
+    private String data;
+
+    public DataParser(){
+        data = "123";
+    }
+
+    public String getData() {
+        return data;
+    }
+
+
+}
