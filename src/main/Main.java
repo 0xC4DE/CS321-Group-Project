e25eb90b1d2da783822297b31a145266cc71b448
Josh <joshuacantrell100@gmail.com>
2022-03-11 14:01:17 -0600

    Static Instances exist

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,15 @@ import java.io.IOException;
 public class Main {
     public static void main(String []args) throws IOException {
         MovieContainer cont = new MovieContainer();
+        MovieContainer.setInstance(cont);
         if(cont.collectMovies(null)){
-
+            System.out.println(MovieContainer.getInstance().getMovieList());
+            System.out.println(cont.getMovieList());
+            if (MovieContainer.getInstance().getMovieList() == cont.getMovieList()){
+                System.out.println("foo");
+            } else {
+                System.out.println("bar");
+            }
         }
     }
 }
