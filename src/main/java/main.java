7c6c4de35dec5684ea762db804c468aaea9cb3fc
Joshua Cantrell <jc0248@uah.edu>
2022-03-11 11:24:03 -0600

    Made simple changes

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,4 @@
-package main;
+package src.main.java;
 
 import java.io.IOException;
 
@@ -6,6 +6,6 @@ public class main {
     public static void main(String []args) throws IOException {
         //Map<?, ?> movie = MovieCollection.get(0);
         //System.out.println(movie);
-      //  Movies movies = new Movies();
+        //Movies movies = new Movies();
     }
 }
