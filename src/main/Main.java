05c21abb26f07ae720c66528d82e1adfc269fa41
Josh <joshuacantrell100@gmail.com>
2022-03-11 14:28:01 -0600

    Added like 5 lines

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String []args) throws IOException {
+        MovieContainer cont = new MovieContainer();
+        cont.collectMovies(null);
+
+        // TODO: Make GSON actually eat the movie data instead of dying.
+        //System.out.println(cont.getMovieList().get(0).getTitle());
+
+
         new TestConstantMovieContainer();
         new TestConstantMovieContainer();
+
+
     }
 }
