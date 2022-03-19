f833d6ded368db6e773d68c529558f9152b96fa3
Emily Burns <emilyburns@Emilys-MacBook-Pro.local>
2022-03-19 14:29:22 -0500

    added dashboard class of tabbed pane

---
+++ src/main/client/Dashboard.java
@@ -0,0 +1,33 @@
+package main.client;
+
+import javax.swing.*;
+
+public class Dashboard {
+
+    /**
+     *
+     */
+    Dashboard() {
+        JFrame window =new JFrame("Dashboard");
+
+        JPanel searchPanel = new JPanel();
+        JPanel wishlistPanel = new JPanel();
+        JPanel profilePanel = new JPanel();
+
+        JTabbedPane tabPanel = new JTabbedPane();
+        tabPanel.setBounds(50,50,200,200);
+
+        tabPanel.add("Search", searchPanel);
+        tabPanel.add("Wishlists", wishlistPanel);
+        tabPanel.add("Profile", profilePanel);
+
+        window.add(tabPanel);
+        window.setSize(400,400);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Dashboard d = new Dashboard();
+    }
+}
