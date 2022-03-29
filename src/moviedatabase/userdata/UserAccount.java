c2b8a13ab9c9901b9ca7bf56795b5725c009d7ad
Josh <joshuacantrell100@gmail.com>
2022-03-29 00:40:51 -0500

    added stuff

--- src/moviedatabase/userdata/UserAccount.java
+++ src/moviedatabase/userdata/UserAccount.java
@@ -1,4 +1,40 @@
 package moviedatabase.userdata;
 
-public interface UserAccount {
+import java.nio.file.Path;
+
+public class UserAccount {
+
+    private static UserAccount instance;
+    private User user;
+
+    /*
+    Default user account is null
+     */
+    private UserAccount() {
+        user = null;
+        instance = this;
+    }
+
+    public static UserAccount getInstance(){
+        return instance;
+    }
+
+    public Boolean changeName(String newName){
+        user.setUsername(newName);
+        return true;
+    }
+
+    public void createAccount(String username, String password, Path userFile){
+        this.user = User.createUser(username, password, userFile);
+    }
+
+    public void login(String username, String password, Path userFile){
+        // ensure filepath exists
+        this.user = user.getUser(username, password, null);
+    }
+
+    public void loginAsGuest(){
+        user = new User();
+    }
+
 }
