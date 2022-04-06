0bf38e588d6867967a3b5ed8bb3898be807bad62
Josh <joshuacantrell100@gmail.com>
2022-04-06 14:00:31 -0500

    Users Actually save to file

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -1,10 +1,10 @@
 package test;
 
 public abstract class Tester {
-    public Tester() {
+    public Tester() throws Exception {
         System.out.println(getClass().getName());
         run();
     }
 
-    public abstract void run();
+    public abstract void run() throws Exception;
 }
