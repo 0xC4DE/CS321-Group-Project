2cdf4334046bd5a37064c9a9fada871bea9499ba
cameron <cjb0052@uah.edu>
2022-04-11 15:24:52 -0500

    added prototype for moving movies between lists

--- src/main/client/WishlistView.java
+++ src/main/client/WishlistView.java
@@ -14,11 +14,13 @@ import java.awt.*;
 
 import java.awt.Insets;
 import java.awt.Dimension;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WishlistView extends Frame {
-
+    private List<ArrayList<Movie>> wishLists;
+    ArrayList<JButton> buttons = new ArrayList<JButton>();
     public WishlistView () {
         ArrayList<JButton> buttons = new ArrayList<JButton>();
        // List<ArrayList<Movie>> test = UserAccount.getInstance().getWishlist();
@@ -43,8 +45,26 @@ public class WishlistView extends Frame {
         setSize(400,400);
         setVisible(true);
     }
+    private void moveList(Movie movieToMove, int newList, int oldList, int oldListIndex){
+        //removing movie from previous list
+        wishLists.get(oldList).remove(oldListIndex);
+        wishLists.get(newList).add(movieToMove);
+        //adds a new button that corrosponds to the moved movie
+        buttons.add(new JButton(movieToMove.getTitle()));
+        //adds the action that clicking will bring up a single movie view of the movie
+        buttons.get(buttons.size()-1).addActionListener(e->{
+            SingleMovieView hey = new SingleMovieView();
+            hey.show(movieToMove);
+        });
+        //This should basically remake the whole view with the changed movie
+        setLayout (new BoxLayout (this, BoxLayout.Y_AXIS));
+        setSize(400,400);
+        setVisible(true);
+    }
+
     // main method
     public static void main(String args[]){
+        //maybe do a for loop, change the view to do one wishlist at a time
         MovieContainer cont = MovieContainer.getInstance();
         cont.collectMovies(null);
         WishlistView b=new WishlistView();
