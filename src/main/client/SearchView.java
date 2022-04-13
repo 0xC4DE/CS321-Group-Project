31d2e7eb152502d070e45608fe6989c16cd3c1c3
cameron <cjb0052@uah.edu>
2022-04-13 12:23:54 -0500

    set up simple search box, can only search by name right now

--- src/main/client/SearchView.java
+++ src/main/client/SearchView.java
@@ -5,18 +5,36 @@ import moviedatabase.moviedata.MovieContainer;
 import moviedatabase.moviesearch.Search;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchView {
+public class SearchView extends Frame {
     public void SearchBox() {
-        List<Movie> foundMovies;
-        JFrame myFrame = new JFrame();
+        /*JFrame myFrame = new JFrame();
         JTextField test = new JTextField(35);
         test.addActionListener(e -> {
+            List<Movie> foundMovies;
             Search searchFor = new Search();
             String text = test.getText();
-            searchFor.searchByName(text);
+            foundMovies = searchFor.searchByName(text);
+            showMovies(foundMovies);
+
+        });
+        myFrame.add(test);
+        setSize(400,400);
+        setVisible(true);*/
+        JFrame myFrame = new JFrame();
+        JTextField textField = new JTextField(20);
+        myFrame.add(textField);
+        myFrame.setSize(100,10);
+        myFrame.setVisible(true);
+        textField.addActionListener(e -> {
+            List<Movie> foundMovies;
+            Search searchFor = new Search();
+            String text = textField.getText();
+            foundMovies = searchFor.searchByName(text);
+            showMovies(foundMovies);
 
         });
     }
@@ -33,7 +51,6 @@ public class SearchView {
                 SingleMovieView hey = new SingleMovieView();
                 hey.show(testMovie);
             });
-
             // adding the buttons so that it can be displayed
             add(buttons.get(j));
 
@@ -44,6 +61,10 @@ public class SearchView {
 
     }
 
+    public static void main(String[] args) {
+        SearchView test = new SearchView();
+        test.SearchBox();
+    }
 
 
 }
