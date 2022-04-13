556eaa4dc02a1f8a8e8592e05d5bbe360e2c71c0
cameron <cjb0052@uah.edu>
2022-04-13 10:19:14 -0500

    changed file locations to paths, config.properties is in the base directory of our repo, and the default locations for the data files is user.home

--- src/main/BootstrapProgram.java
+++ src/main/BootstrapProgram.java
@@ -1,42 +1,42 @@
 package main;
-import moviedatabase.moviedata.Movie;
 import moviedatabase.moviedata.MovieContainer;
 
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.FileNotFoundException;
 import java.util.Properties;
-import java.util.Scanner;
 
 public class BootstrapProgram {
-    private String movieFile;
-    private String configFile;
-    private String reviewPath;
-    public void BoostrapProgram(){
-        try(FileInputStream in = new FileInputStream("/resources/config.properties")) {
+    private Path moviePath;
+    private Path configFile;
+    private Path reviewPath;
+    public BootstrapProgram(){
+        Path test = Paths.get(System.getProperty("user.home"));
             Properties props = new Properties();
-            props.load(in);
-            props.setProperty("moviePath", "../../data/data.json");
-            props.setProperty("reviewPath", "../../data/reviews.json");
-        }
-        catch (IOException e){
+            props.setProperty("moviePath", String.valueOf(test));
+            props.setProperty("reviewPath", String.valueOf(test));
+        try {
+            props.store(new FileWriter("config.properties"),"This contains the file locations of our data");
+            configFile = Paths.get("config.properties");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void setConfigFile(String newMoviePath, String newReviewPath) throws IOException {
-        FileInputStream in = new FileInputStream(configFile);
+        FileInputStream in = new FileInputStream(String.valueOf(configFile));
         Properties props = new Properties();
         props.load(in);
         props.setProperty("moviePath",newMoviePath);
         props.setProperty("reviewPath",newReviewPath);
     }
     public boolean loadConfig()  {
-       try( FileInputStream in = new FileInputStream(configFile)) {
+       try( FileInputStream in = new FileInputStream(String.valueOf(configFile))) {
            Properties configInfo = new Properties();
            configInfo.load(in);
-           movieFile = configInfo.getProperty("moviePath");
-           reviewPath = configInfo.getProperty("reviewPath");
+           moviePath = Path.of(configInfo.getProperty("moviePath"));
+           reviewPath = Path.of(configInfo.getProperty("reviewPath"));
            return true;
        }
        catch (IOException e){
@@ -46,8 +46,12 @@ public class BootstrapProgram {
     }
     public void loadMovies(){
         MovieContainer temp = MovieContainer.getInstance();
-        temp.collectMovies(Path.of(movieFile));
+        temp.collectMovies(moviePath);
     }
-
+    public static void main(String []args)  {
+        BootstrapProgram test = new BootstrapProgram();
+        test.loadConfig();
+        System.out.println(test.moviePath);
+    }
 
 }
