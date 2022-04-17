0e3eed6ff65b81562f0c31c2a368b3d1736e34fb
Erik <edo0005@uah.edu>
2022-04-16 19:03:08 -0500

    ignore my recent commit to main, added those details into this branch, and replaced the searchbydirector for similar with searchbygenre

--- src/main/client/SingleMovieView.java
+++ src/main/client/SingleMovieView.java
@@ -11,19 +11,19 @@ import java.util.List;
 class SingleMovieView {
     public void show(Movie movieToShow) {
         JFrame jFrame = new JFrame();
-        JOptionPane.showMessageDialog(jFrame, "Title: "+movieToShow.getTitle()+"\n"+movieToShow.getDirector()+"\n"+
-        movieToShow.getRated()+"\n"+movieToShow.getCountry()+"\n"+movieToShow.getGenre()+"\n"+movieToShow.getLanguage()+"\n"+
-               movieToShow.getRuntime()+"\n"+movieToShow.getYear()+"\n\tSimilar:  "+findSimilar(movieToShow));
+        JOptionPane.showMessageDialog(jFrame, "Title: "+movieToShow.getTitle()+"\nDirector: "+movieToShow.getDirector()+"\nRated: "+
+        movieToShow.getRated()+"\nCountry:"+movieToShow.getCountry()+"\nGenre: "+movieToShow.getGenre()+"\nLanguage: "+movieToShow.getLanguage()+"\nRuntime: "+
+               movieToShow.getRuntime()+"\nYear: "+movieToShow.getYear()+"\n\tSimilar:  "+findSimilar(movieToShow));
 
     }
     public String findSimilar(Movie base){
         List<Movie> results;
         Search test = new Search();
         String similar=" ";
-        results = test.searchByDirector(base.getDirector());
-            for(int  i=0;i<results.size();i++){
-                similar+=results.get(i).getTitle()+", ";
-            }
+        results = test.searchByGenre(base.getGenre());
+        for (Movie result : results) {
+            similar += result.getTitle() + ", ";
+        }
         return similar;
 
     }
